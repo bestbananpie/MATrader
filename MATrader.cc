3c6c7062de583d21076386ae9178261174ee9848
Simon Lee <silee001@gmail.com>
2014-08-08 10:48:33 +0100

    Initial Upload

---
+++ MATrader.cc
@@ -0,0 +1,31 @@
+/*
+ * @file MATrader.mq4
+ * @author Simon Lee (silee001@gmail.com)
+ * @brief Trades Forex based on Long and short term trends
+ *
+ */
+
+#property copyright "Simon Lee"
+#property link      ""
+#property version   "1.00"
+#property strict
+
+/* ************************************************************************** */
+/* Initialise                                                                 */
+int OnInit() {
+  return(INIT_SUCCEEDED);
+}
+
+
+/* ************************************************************************** */
+/* On Tick                                                                    */
+void OnTick() {
+
+}
+
+
+/* ************************************************************************** */
+/* Deinitialise                                                               */
+void OnDeinit(const int reason) {
+
+}
