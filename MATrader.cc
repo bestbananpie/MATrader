1c6c44186404498cdb1fdb908c3e1f54dec89b8d
Simon Lee <silee001@gmail.com>
2014-08-08 12:36:54 +0100

    Added MA Chart Indicators. Need to have them removed

--- MATrader.cc
+++ MATrader.cc
@@ -1,5 +1,5 @@
 /*
- * @file MATrader.mq4
+ * @file MATrader.mq5
  * @author Simon Lee (silee001@gmail.com)
  * @brief Trades Forex based on Long and short term trends
  *
@@ -11,10 +11,41 @@
 #property strict
 
 /* ************************************************************************** */
+/* Input                                                                      */
+input int MA_Long  = 30;
+input int MA_Short = 10;
+
+
+int iMA_Handle_Long = 0;
+int iMA_Handle_Short = 0;
+/* ************************************************************************** */
 /* Initialise                                                                 */
 int OnInit() {
-  return(INIT_SUCCEEDED);
+  bool init_success = true;
+
+  /* ************************************************************************ */
+  /* Add MA Indicators                                                        */
+  iMA_Handle_Long = iMA(Symbol(),0,MA_Long,0,MODE_SMA,PRICE_MEDIAN);
+    // Symbol name, Period, Averaging period, Horizontal shift, Smoothing type, Type of price or handle
+  ChartIndicatorAdd(0,0,iMA_Handle_Long);
+    // Chart ID, Number of Subwindow, Handle of the indicator
+
+  iMA_Handle_Short = iMA(Symbol(),0,MA_Short,0,MODE_SMA,PRICE_MEDIAN);
+    // Symbol name, Period, Averaging period, Horizontal shift, Smoothing type, Type of price or handle
+  ChartIndicatorAdd(0,0,iMA_Handle_Short);
+    // Chart ID, Number of Subwindow, Handle of the indicator
+
+  if(init_success == true) {
+    return(INIT_SUCCEEDED);
+  }
+
+  else {
+    Alert("Error in Initialising");
+    return(INIT_FAILED);
+  }
+
 }
+
 
 
 /* ************************************************************************** */
@@ -27,5 +58,6 @@ void OnTick() {
 /* ************************************************************************** */
 /* Deinitialise                                                               */
 void OnDeinit(const int reason) {
-
+   ChartIndicatorDelete(0,0,iMA_Handle_Long);
+   ChartIndicatorDelete(0,0,iMA_Handle_Short);
 }
--- README.md
+++ README.md
@@ -2,3 +2,9 @@ MATrader
 ========
 
 Moving Average Trader for MetaTrader 4
+
+
+Installation
+-----------
+The file extension should be renamed to a .mq4 type
+
